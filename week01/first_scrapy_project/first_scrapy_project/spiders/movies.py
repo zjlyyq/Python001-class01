9cd88174cbefb6ceac30d1aa45919d0506f598c6
jialuzhang <jialuzhang0805@gmail.com>
2020-06-27 15:59:43 +0800

    使用scrapy自带的Xpath代替bs4提取页面内容

--- week01/first_scrapy_project/first_scrapy_project/spiders/movies.py
+++ week01/first_scrapy_project/first_scrapy_project/spiders/movies.py
@@ -1,6 +1,7 @@
 import scrapy
-from bs4 import BeautifulSoup as bs
-from first_scrapy_project.items import FirstScrapyProjectItem
+# from first_scrapy_project.items import FirstScrapyProjectItem
+from  scrapy.selector import Selector
+
 class MoviesSpider(scrapy.Spider):
     name = 'movies'
     allowed_domains = ['movie.douban.com']
@@ -9,35 +10,24 @@ class MoviesSpider(scrapy.Spider):
 
     def start_requests(self):
         for i in range(1):
+            print(i)
             url = f'https://movie.douban.com/top250?start={i*25}'
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse, dont_filter=False)
 
-            
-    # def parse(self, response):
-    #     pass
+
 
     def parse(self, response):
-        items = []
-        bs_info = bs(response.text, 'html.parser')
-        for div_tag in bs_info.find_all('div', attrs={"class": "hd"}):
-            # print('共有电影' + str(len(div_tag)) + "部")
-            for a_tag in div_tag.find_all('a'):
-                item = FirstScrapyProjectItem()
-                # 获取电影链接
-                link = a_tag.get('href')
-                # # 获取a标签下第一个span——电影名
-                title = a_tag.find('span',).text
-                item['title'] = title
-                item['link'] = link
-                yield scrapy.Request(url=link, meta={'item': item}, callback=self.parse2)
-        # return items
-
-    # 处理电影详情页面返回
-    def parse2(self, response):
-        item = response.meta['item']
-        bs_detail_info = bs(response.text, 'html.parser')
-        content = bs_detail_info.find('div', attrs={'class': 'related-info'}).get_text().strip()
-        item['content'] = content
-        yield item
-
-
+        movies = Selector(response=response).xpath('//div[@class="hd"]')
+        for movie in movies:
+            # 路径使用 / .  .. 不同的含义　
+            title = movie.xpath('./a/span/text()')
+            link = movie.xpath('./a/@href')
+            print(title)
+            print(link)
+            print(title.extract())
+            print(link.extract())
+            print(title.extract_first())
+            print(link.extract_first())
+            print(title.extract_first().strip())
+            print(link.extract_first().strip())
+        
