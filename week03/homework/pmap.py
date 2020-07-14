499cc297ce64a76652129e3be276c790565cda6b
jialuzhang <jialuzhang0805@gmail.com>
2020-07-14 14:32:25 +0800

    week03

--- week03/homework/pmap.py
+++ week03/homework/pmap.py
@@ -17,23 +17,14 @@ parser.add_argument('-ip', default="4", required=True, nargs="?", help="ip range
 parser.add_argument('-w', default="result.json", nargs="?", help="result writen to a file")
 
 # ip, ip_status, lock
-def ping_by_ping3(ip, p_id="0001"):
-    time.sleep(1)
-    print(f'进程{p_id}开始，ip = {ip} 进程号：{os.getpid()}')
-    r = ping3.ping(ip)
-    time.sleep(1)
-    print(r)
-    # lock.acquire()
-    # if r:
-    #     ip_status.put({ip: "arrival"})
-    # else:
-    #     ip_status.put({ip: "not arrival"})
-    # lock.release()
-    print(f'进程{p_id}结束， 进程号：{os.getpid()}')
+def ping_by_ping3(t):
+    print(f'进程{t[0]}开始，ip = {t[1]} 进程号：{os.getpid()}')
+    r = ping3.ping(t[1])
+    print(f'进程{t[1]}结束， 进程号：{os.getpid()}')
     if r:
-        return {ip: 'arrival'}
+        return {t[1]: 'arrival'}
     else:
-        return {ip: 'not arrival'}
+        return {t[1]: 'not arrival'}
 if __name__ == "__main__": 
     argc = parser.parse_args()
     ip_range = argc.ip.split('-')
@@ -50,9 +41,9 @@ if __name__ == "__main__":
                 for ip4 in range(int(ip_range[0].split('.')[3]), int(ip_range[1].split('.')[3])+1):
                     cur_ip = str(ip1) + '.' + str(ip2) + '.' + str(ip3) + '.' + str(ip4)
                     process_id += 1
-                    ip_list.append((cur_ip,  process_id ))
-    print(ip_list)
+                    ip_list.append((process_id,cur_ip))
     result = p.map(ping_by_ping3, ip_list)
+    print(type(result))
     p.close()
     p.join()
     p.terminate()
